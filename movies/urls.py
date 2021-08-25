957e1b39141c11d921128537ded187be8529436e
AlexStash666 <alexanderstashinski@gmail.com>
2021-08-25 19:46:57 +0300

    Rating. Refresh movies rating

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('movie/', views.MovieListView.as_view()),
     path('movie/<int:pk>/', views.MovieDetailView.as_view()),
     path('review/', views.ReviewCreateView.as_view()),
+    path('rating/', views.AddStarRatingView.as_view()),
 ]
--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 
 
 from .models import Movie
-from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer
+from .serializers import MovieListSerializer, MovieDetailSerializer, \
+    ReviewCreateSerializer, CreateRatingSerializer
 
 
 class MovieListView(APIView):
@@ -29,3 +30,22 @@ class ReviewCreateView(APIView):
         if review.is_valid():
             review.save()
         return Response(status=201)
+
+
+class AddStarRatingView(APIView):
+    """Добавление рейтинга к фильму"""
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    def post(self, request):
+        serializer = CreateRatingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(ip=self.get_client_ip(request))
+            return Response(status=201)
+        else:
+            return Response(status=400)
