b57de490adc74cdaa7fe88fad81a9dd8b548a1cd
AlexStash666 <alexanderstashinski@gmail.com>
2021-08-28 12:18:58 +0300

    drf-yasg. Auto-doc

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models import Avg
-from rest_framework import generics
+from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Movie, Actor
@@ -20,6 +20,7 @@ class MovieListView(generics.ListAPIView):
     serializer_class = MovieListSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = MovieFilter
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         movies = Movie.objects.filter(draft=False).annotate(
